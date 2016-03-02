3610b3f98d29fc49b74a8cf7d628e617c066a44c
Rodrigo Ortiz Cayon <rodrigo.ortiz-cayon@inria.fr>
2016-03-02 18:43:18 +0100

    flow

--- release/main_run_siftFLow.m
+++ release/main_run_siftFLow.m
@@ -1,11 +1,8 @@
-
 %%
 
 addpath('C:\Users\rortizca\workspace\ibr-common\src\utilities\scripts\scripts_plane_estimation\readwrite');
-
 addpath(fullfile(pwd,'mexDenseSIFT'));
 addpath(fullfile(pwd,'mexDiscreteFlow'));
-
 
 %%
 %DATASET = 'D:\D_workspace\datasets\0_debuging\0_2labels\hugo1-24\big_spixels';
@@ -14,11 +11,10 @@ DATASET = 'D:\D_workspace\datasets\0_debuging\0_2labels\hugo1-24\IBR_data_VSFM-C
 %DATASET = 'D:\D_workspace\datasets\0_debuging\0_2labels\hugo2-25\big_spixels';
 %DATASET = 'D:\D_workspace\datasets\0_debuging\0_2labels\hugo2-25\IBR_data_VSFM-CMPMVS';
 
-
 %DATASET = 'D:\D_workspace\datasets\0_debuging\0_2labels\yellow_house\big_spixels';
 %DATASET = 'D:\D_workspace\datasets\0_debuging\0_2labels\beausoleil-9\big_spixels';
 
-IDs         = [0, 24];
+IDs     = [0, 24];
 
 %%
 
@@ -51,5 +47,3 @@ for id = IDs(1):IDs(2)-1
     pause;
     
 end
-
-
